602206524ff8c7098ad252e35c27396f51db421b
Balder <balder.ansgar@gmail.com>
2025-10-20 10:25:33 -0300

    Add função adicionar_produtos ✔

--- back-end/funcao.py
+++ back-end/funcao.py
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS produtos (
 id SERIAL PRIMARY KEY,
 nome VARCHAR(100) NOT NULL,
 categoria VARCHAR(50),
-preco DECIMAL(10,2),
+preco NUMERIC(10,2),
 quantidade INT
 )
 """)
@@ -18,4 +18,16 @@ quantidade INT
         finally:
             cursor.close()
             conexao.close()
-criar_tabela()
+
+def adicionar_produto(nome, categoria, preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("INSERT INTO produtos (nome, categoria, preco, quantidade) VALUES (%s, %s, %s, %s)",
+                           (nome, categoria, preco, quantidade))
+            conexao.commit()
+        except Exception as error:
+            print(f"Erro ao inserir filme: {error}")
+        finally:
+            cursor.close()
+            conexao.close()
