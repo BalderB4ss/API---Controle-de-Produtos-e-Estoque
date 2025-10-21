153d78b27b47af0718340391b9443f9aba7d7be4
Balder <balder.ansgar@gmail.com>
2025-10-21 16:51:42 -0300

    Dando erro ainda

--- front-end/app.py
+++ front-end/app.py
@@ -10,7 +10,7 @@ if response.status_code == 200:
 
 st.set_page_config(page_title="Controle de Produtos e Estoque", page_icon="🚚")
 st.title("🚛 Controle de Produtos e Estoque")
-menu = st.sidebar.radio("Navegação", ["Catálogo","Adicionar Produto","Apagar Produto","Atualizar Produto"])
+menu = st.sidebar.radio("Navegação", ["Catálogo","Adicionar Produto","Apagar Produto","Atualizar Preço","Atualizar Quantidade"])
 if menu =="Catálogo":
     st.subheader("Todos os produtos disponíveis")
     response = requests.get(f"{API_URL}/estoque")
@@ -47,7 +47,7 @@ if menu == "Adicionar Produto":
 if menu == "Apagar Produto":
     st.subheader("❌ Apagar Produto ❌")
     if ids:
-        escolha = st.selectbox("Escolha um produto para apagar",ids)
+        escolha = st.selectbox("Escolha o ID do produto para apagar",ids)
         response = requests.get(f"{API_URL}/estoque")
         if response.status_code == 200:
             produtos = response.json().get("produtos",[])
@@ -64,3 +64,28 @@ if menu == "Apagar Produto":
                 st.error("Erro ao deletar produto❗")
     else:
         st.info("Nenhum produto disponível para deletar!")
+
+if menu == "Atualizar Preço":
+    st.subheader("📦 Atualizar Preço do Produto 📦")
+    if not ids:
+        st.warning("Nenhum produto disponível para atualizar")
+    else:
+        escolha = st.selectbox("Selecione o ID do produto para atualizar",ids)
+        response = requests.get(f"{API_URL}/estoque")
+        if response.status_code == 200:
+            produtos = response.json().get("produtos"[0], [])
+            produto_selecionado = next((item for item in produtos if item["id"] == escolha), None)
+            if produto_selecionado:
+                st.dataframe([produto_selecionado])
+            else:
+                st.error("Erro ao buscar detalhes do produto")
+            preco = st.number_input("Novo preço (para não atualizar deixe 0)", value=0, format="%.2f")
+            if st.button("Atualizar Produto ✔"):
+                dados = {
+                    "preco": preco,
+                }
+                response = requests.put(f"{API_URL}/estoque/i{escolha}", params=dados)
+                if response.status_code == 200:
+                    st.success("Produto atualizado com sucesso!")
+                else:
+                    st.error("Erro ao atualizar produto❗")
